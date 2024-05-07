267051bfde19e01edbc17558adf9d50a7989cf77
LinusDev <linus@linusdev.de>
2024-05-07 17:15:01 +0200

    mp

--- question.php
+++ question.php
@@ -42,9 +42,14 @@ defined('MOODLE_INTERNAL') || die();
 
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class qtype_regexmatch_question extends question_graded_automatically_with_countback {
+class qtype_regexmatch_question extends question_graded_automatically {
 
-    public function start_attempt(question_attempt_step $step, $variant) {
+    public string $regex = "";
+
+    public function start_attempt(
+        question_attempt_step $step,
+        $variant
+    ) {
         // probably not needed
     }
 
@@ -65,87 +70,45 @@ class qtype_regexmatch_question extends question_graded_automatically_with_count
             $prevresponse, $newresponse, 'answer');
     }
 
-    // Template stuff
-    // Template stuff
-    // Template stuff
-    // Template stuff
-    // Template stuff
-    // Template stuff
-
-
-
-    /* it may make more sense to think of this as get expected data types */
     public function get_expected_data() {
-        // TODO.
-        return array();
+        return array('answer' => PARAM_RAW);
     }
 
-
-
-    /**
-     * @return summary
-     * A string that summarises how the user responded.
-     * It is written to responsesummary field of
-     * the question_attempts table, and used in the
-     * the quiz responses report
-     * */
     public function summarise_response(array $response) {
-        // TODO.
+        return $response['answer'] ?? null;
+    }
+
+    public function un_summarise_response(string $summary): array {
+        if (!empty($summary)) {
+            return ['answer' => $summary];
+        } else {
+            return [];
+        }
+    }
+
+    public function grade_response(array $response): array {
+        $submittedAnswer = $response['answer'] ?? null;
+        $fraction = 0;
+
+        if($submittedAnswer == null)
+            $fraction = 0;
+        else if(preg_match("/" . str_replace("/", "\\/", $this->regex) . "/", $submittedAnswer) == 1)
+            $fraction = 1;
+
+        return array($fraction, question_state::graded_state_for_fraction($fraction));
+    }
+
+    public function get_correct_response() {
         return null;
     }
 
-
-
-    /**
-     * if you are moving from viewing one question to another this will
-     * discard the processing if the answer has not changed. If you don't
-     * use this method it will constantantly generate new question steps and
-     * the question will be repeatedly set to incomplete. This is a comparison of
-     * the equality of two arrays.
-     * Comment from base class:
-     *
-     * Use by many of the behaviours to determine whether the student's
-     * response has changed. This is normally used to determine that a new set
-     * of responses can safely be discarded.
-     *
-     * @param array $prevresponse the responses previously recorded for this question,
-     *      as returned by {@link question_attempt_step::get_qt_data()}
-     * @param array $newresponse the new responses, in the same format.
-     * @return bool whether the two sets of responses are the same - that is
-     *      whether the new set of responses can safely be discarded.
-     */
-
-
-
-     /**
-     * @return question_answer an answer that
-     * contains the a response that would get full marks.
-     * used in preview mode. If this doesn't return a
-     * correct value the button labeled "Fill in correct response"
-     * in the preview form will not work. This value gets written
-     * into the rightanswer field of the question_attempts table
-     * when a quiz containing this question starts.
-     */
-    public function get_correct_response() {
-        // TODO.
-        return array();
-    }
-    /**
-     * Given a response, reset the parts that are wrong. Relevent in
-     * interactive with multiple tries
-     * @param array $response a response
-     * @return array a cleaned up response with the wrong bits reset.
-     */
     public function clear_wrong_from_response(array $response) {
-        foreach ($response as $key => $value) {
-            /*clear the wrong response/s*/
-        }
+        // We want to keep the previous answer as it is only a single answer field
         return $response;
     }
 
     public function check_file_access($qa, $options, $component, $filearea,
             $args, $forcedownload) {
-        // TODO.
         if ($component == 'question' && $filearea == 'hint') {
             return $this->check_hint_file_access($qa, $options, $args);
 
@@ -153,45 +116,5 @@ class qtype_regexmatch_question extends question_graded_automatically_with_count
             return parent::check_file_access($qa, $options, $component, $filearea,
                     $args, $forcedownload);
         }
-    }
-    /**
-     * @param array $response responses, as returned by
-     *      {@link question_attempt_step::get_qt_data()}.
-     * @return array (number, integer) the fraction, and the state.
-     */
-    public function grade_response(array $response) {
-        // TODO.
-        $fraction = 0;
-        return array($fraction, question_state::graded_state_for_fraction($fraction));
-    }
-
-     /**
-     * Work out a final grade for this attempt, taking into account all the
-     * tries the student made. Used in interactive behaviour once all
-     * hints have been used.     *
-     * @param array $responses an array of arrays of the response for each try.
-     * Each element of this array is a response array, as would be
-     * passed to {@link grade_response()}. There may be between 1 and
-     * $totaltries responses.
-     * @param int $totaltries is the maximum number of tries allowed. Generally
-     * not used in the implementation.
-     * @return numeric the fraction that should be awarded for this
-     * sequence of response.
-     *
-     */
-    public function compute_final_grade($responses, $totaltries) {
-        /*This method is typically where penalty is used.
-        When questions are run using the 'Interactive with multiple
-        tries or 'Adaptive mode' behaviour, so that the student will
-        have several tries to get the question right, then this option
-        controls how much they are penalised for each incorrect try.
-
-        The penalty is a proportion of the total question grade, so if
-        the question is worth three marks, and the penalty is 0.3333333,
-        then the student will score 3 if they get the question right first
-        time, 2 if they get it right second try, and 1 of they get it right
-        on the third try.*/
-        //TODO
-        return 0;
     }
 }
--- renderer.php
+++ renderer.php
@@ -36,17 +36,40 @@ defined('MOODLE_INTERNAL') || die();
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class qtype_regexmatch_renderer extends qtype_renderer {
-    public function formulation_and_controls(question_attempt $qa,
-            question_display_options $options) {
+    public function formulation_and_controls(
+        question_attempt $qa,
+        question_display_options $options
+    ): string {
 
+        // regexmatch question
         $question = $qa->get_question();
 
+        // Text to be displayed for this question (set when creating)
         $questiontext = $question->format_questiontext($qa);
-       
-        $result = html_writer::tag('div', $questiontext, array('class' => 'qtext'));
-        /* Some code to restore the state of the question as you move back and forth
-        from one question to another in a quiz and some code to disable the input fields
-        once a quesiton is submitted/marked */
+
+        // The last answer, that the student entered (if any)
+        $currentanswer = $qa->get_last_qt_var('answer');
+
+        $result = "";
+
+        // Add question text
+        $result .= html_writer::tag('div', $questiontext, array('class' => 'qtext'));
+
+        // Add input field
+        $inputname = $qa->get_qt_field_name('answer');
+        $inputattributes = array(
+            'type' => 'text',
+            'name' => $inputname,
+            'value' => $currentanswer,
+            'id' => $inputname,
+            'size' => 80,
+            'class' => 'form-control d-inline',
+        );
+
+        if ($options->readonly)
+            $inputattributes['readonly'] = 'readonly';
+
+        $result .= html_writer::empty_tag('input', $inputattributes);
 
         /* if ($qa->get_state() == question_state::$invalid) {
             $result .= html_writer::nonempty_tag('div',
